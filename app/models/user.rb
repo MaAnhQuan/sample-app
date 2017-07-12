f7a50d49b93497cba7cbdc5bf5bdd5cd15676522
MaAnhQuan <ma.quan.1711@gmail.com>
2017-07-12 16:51:47 +0700

    Sign Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
-  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save :downcase
+
+  validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
+
+  has_secure_password
+
+  private
+
+  def downcase
+    email.downcase!
+  end
 end
