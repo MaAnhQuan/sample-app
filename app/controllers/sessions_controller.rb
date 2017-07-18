6cc23e39681fae6c4badca79a62c053dcb926842
MaAnhQuan <ma.quan.1711@gmail.com>
2017-07-18 12:31:43 +0700

    Update, show, destroy user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,6 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def login_success user
-    log_in user
-    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-    redirect_to user
-  end
-
-  def login_fail
-    flash.now[:danger] = t "login_error"
-    render :new
-  end
-
   def create
     session = params[:session]
     user = User.find_by email: session[:email].downcase
@@ -25,5 +14,18 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def login_success user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_back_or user
+  end
+
+  def login_fail
+    flash.now[:danger] = t "login_error"
+    render :new
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,13 +10,6 @@ module SessionsHelper
     cookie[:remember_token] = user.remember_token
   end
 
-  def cookies_current cookies_id
-    user = User.find_by id: cookies_id
-    return unless user && user.authenticated?(cookies[:remember_token])
-    log_in user
-    @current_user = user
-  end
-
   def current_user
     session_id = session[:user_id]
     cookies_id = cookies.signed[:user_id]
@@ -25,6 +18,10 @@ module SessionsHelper
     elsif cookies_id
       cookies_current cookies_id
     end
+  end
+
+  def current_user? user
+    user == current_user
   end
 
   def logged_in?
@@ -41,5 +38,24 @@ module SessionsHelper
     forget current_user
     session.delete :user_id
     @current_user = nil
+  end
+
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  private
+
+  def cookies_current cookies_id
+    user = User.find_by id: cookies_id
+
+    return unless user && user.authenticated?(cookies[:remember_token])
+    log_in user
+    @current_user = user
   end
 end
